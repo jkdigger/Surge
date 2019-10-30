ef633cc7725c591d0d5f8fdd8160fd32aed29150
jkdigger <jkdigger@163.com>
2019-10-30 21:29:34 +0800

    edit

--- script/luqiStory.js
+++ script/luqiStory.js
@@ -2,3 +2,10 @@ let obj = JSON.parse($response.body);
 obj.data.is_vip = 1;
 obj.data.vip_endtime = 1630296877;
 $done({body: JSON.stringify(obj)});
+
+/*
+http-response https:\/\/www\.luqijianggushi\.com\/api\/v2\/user\/get requires-body=1,max-size=0,script-path=script/luqiStory.js
+
+www.luqijianggushi.com
+
+*/
